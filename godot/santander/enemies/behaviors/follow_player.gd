1adfda5e25e30233698cc224768cbab9fcb4cc0a
Allef Rodrigo Schmidt <mac-allef@MacBook-Air-de-Allef.local>
2024-07-18 19:25:12 -0300

    add new things

--- godot/santander/enemies/behaviors/follow_player.gd
+++ godot/santander/enemies/behaviors/follow_player.gd
@@ -1,6 +1,8 @@
 extends Node
 
-@export var speed:float = 1
+
+@export var speed: float = 1
+
 var enemy: Enemy
 var sprite: AnimatedSprite2D
 
@@ -8,18 +10,20 @@ var sprite: AnimatedSprite2D
 func _ready():
 	enemy = get_parent()
 	sprite = enemy.get_node("AnimatedSprite2D")
-	
-	
+
+
 func _physics_process(delta: float) -> void:
+	# Calcular direção
 	var player_position = GameManeger.player_position
 	var difference = player_position - enemy.position
 	var input_vector = difference.normalized()
-	enemy.velocity = input_vector * speed * 100.0
-
-	enemy.move_and_slide()	
 	
+	# Movimento
+	enemy.velocity = input_vector * speed * 100.0
+	enemy.move_and_slide()
+
+	# Girar sprite
 	if input_vector.x > 0:
 		sprite.flip_h = false
-
 	elif input_vector.x < 0:
 		sprite.flip_h = true
--- godot/santander/player/player.gd
+++ godot/santander/player/player.gd
@@ -1,16 +1,23 @@
+class_name Player
 extends CharacterBody2D
 @export var speed: float = 3
 @export var sword_damage: int = 2
 
+@export var health:int = 100
+@export var max_health: int = 100
+@export var death_prefab: PackedScene
+
 @onready var sprite: Sprite2D = $Sprite2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var sword_area: Area2D = $SwordArea
+@onready var hitbox_area: Area2D = $HitboxArea
+
 var input_vector: Vector2 = Vector2(0,0)
 var is_running: bool = false
 var was_running: bool = false
 var is_attacking: bool = false
 var attack_coldown: float = 0.0
-
+var hitbox_coldown: float = 0.0
 
 func _process(delta: float) -> void:
 	GameManeger.player_position = position
@@ -24,6 +31,8 @@ func _process(delta: float) -> void:
 	play_run_idle_animation()
 	if not is_attacking:
 		rotate_sprite()
+		
+	update_hitbox_detection(delta)	
 
 func _physics_process(delta: float) -> void:
 
@@ -92,4 +101,44 @@ func deal_damage_to_enemies() -> void:
 			if dot_product >= 0.6:
 				enemy.damage(sword_damage)
 	
+func damage(amount: int) -> void:
+	if health <= 0: return
+	health -= amount
+	print("Player recebeu dano de: ", amount, "A vida total é de:",health )
+	modulate = Color.RED
+	var tween = create_tween()
+	tween.set_ease(Tween.EASE_IN)
+	tween.set_trans(Tween.TRANS_QUINT)
+	tween.tween_property(self, "modulate", Color.WHITE, 0.3)
+	if health <= 0:
+		die()
 
+func die() -> void:
+	if death_prefab:
+		var death_object = death_prefab.instantiate()
+		death_object.position = position
+		get_parent().add_child(death_object)
+		
+	queue_free()
+		
+
+func update_hitbox_detection(delta: float) -> void:
+	hitbox_coldown -= delta
+	if hitbox_coldown > 0: return
+	
+	hitbox_coldown = 0.5
+	var bodies = hitbox_area.get_overlapping_bodies()
+	for body in bodies:
+		if body.is_in_group("enemies"):
+			var enemy: Enemy = body
+			var damage_amount = 1
+			damage(damage_amount) 
+
+func heal(amount: int) -> int:
+	health += amount
+	if health > max_health:
+		health = max_health
+	print("cura:", amount, " vida:", health)
+	return health
+			
+		
